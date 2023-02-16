b7e1e8a3aec1c16a169b9f9843683af39e0b8a18
jalolomonov <jalolbekomon@gmail.com>
2023-02-16 16:51:54 +0500

    3-day-1

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,59 +1,231 @@
 <script lang="ts">
 	import axios from "axios";
-
+	
 	interface Group {
-		id: string,
-		name: string,
-		direction: string
+		id: string;
+		name: string;
+		direction: string;
 	}
 
-	let groups: Group[] = []
-	let group_name: HTMLInputElement
-	let group_direction: HTMLInputElement
+	let groups: Group[] = [];	
+	let group_name: HTMLInputElement;
+	let group_direction: HTMLInputElement;
+	let i: number = 1
 
 	async function getAllGroups() {
-		await axios
-			.get('http://localhost:7060/groups')
-			.then(res => {
-				groups = res.data.groups
-			})
+		axios.get("http://localhost:7060/groups").then(async (res) => {
+			groups = await res.data.groups;
+		});
 	}
 	async function postGroup(name: string, direction: string) {
 		const group = {
 			name,
-			direction
-		}
-		await axios
-			.post('http://localhost:7060/groups', group)
-			.then(res => {
-				alert(res.data.message)
-				getAllGroups()
-			})
+			direction,
+		};
+		axios.post("http://localhost:7060/groups", group).then(async (res) => {
+			alert(await res.data.message);
+			getAllGroups();
+		});
 	}
-	
-	getAllGroups()
+	async function deleteGroup(id: string) {
+		axios.delete("http://localhost:7060/groups/" + id).then(async (res) => {
+			alert(await res.data.message);
+			getAllGroups();
+		});
+	}
+
+	getAllGroups();
 </script>
 
-<section class="h-screen flex">
-	<div class="flex flex-col gap-2 p-5 h-screen border-r-4">
-		<p class="text-xl font-semibold">Create group</p>
-		<label for="group-name">Enter group name</label>
-		<input bind:this={group_name} class="border-2 p-2 rounded outline-0" type="text" id="group-name" placeholder="Android">
-		<label for="group-direction">Enter group direction</label>
-		<input bind:this={group_direction} class="border-2 p-2 rounded outline-0" type="text" id="group-name" placeholder="Info group">
-		<button on:click={() => {postGroup(group_name.value, group_direction.value)}}  class="bg-indigo-600 p-2 rounded text-white">Create group</button>
+<head>
+	<title>Dashboard</title>
+</head>
+
+<section class="grid grid-cols-4">
+	<div
+		class="aside col-span-1 sticky top-0 left-0 flex flex-col justify-between gap-2 p-5 h-screen bg-white"
+	>
+		<div>
+			<p class="text-2xl font-semibold">Dashboard</p>
+			<div class="flex flex-col gap-3">
+				<a
+					href="/groups"
+					class="text-white text-lg bg-blue-700 rounded-lg px-4 py-1"
+					><i class="bi bi-people-fill" /> Groups</a
+				>
+				<a
+					href="li"
+					class="text-white text-lg bg-blue-700 rounded-lg px-4 py-1"
+					><i class="bi bi-people-fill" /> Teachers</a
+				>
+				<a
+					href="li"
+					class="text-white text-lg bg-blue-700 rounded-lg px-4 py-1"
+					><i class="bi bi-people-fill" /> Students</a
+				>
+			</div>
+		</div>
+		<div class="inline-flex flex-col gap-3">
+			<a
+				href="se"
+				class="text-white text-lg bg-blue-700 rounded-lg px-4 py-1"
+				><i class="bi bi-gear-fill" /> Settings</a
+			>
+			<button class="text-white text-lg bg-red-700 rounded-lg px-4 py-1"
+				><i class="bi bi-box-arrow-in-left" /> Chiqish</button
+			>
+		</div>
 	</div>
-	<div class="p-5 bg-[#e2e8f0] w-full">
-		<p class="text-xl font-semibold">All groups</p>
-		<div class="grid gap-3 grid-cols-4 py-5">
-			{#each groups as group}
-				<div class="flex flex-col justify-start gap-3 rounded-xl shadow p-4 bg-white">
-					<p><b>Id: </b>{group.id}</p>
-					<p><b>Group: </b>{group.name}</p>
-					<p><b>Direction: </b>{group.direction}</p>
-					<button class="text-white bg-red-600 p-2 rounded">O'chirish</button>
+	<div class="col-span-3 p-5 bg-[#e2e8f0] w-full">
+		<div>
+			<p class="text-xl font-semibold">Dashboard</p>
+			<div class="flex gap-5 py-5">
+				<div
+					class="flex flex-col justify-start gap-3 rounded-xl shadow p-4 bg-white"
+				>
+					<p class="text-2xl font-semibold">All groups</p>
+					<div class="flex items-end gap-5">
+						<p class="text-7xl text-blue-500">
+							<i class="bi bi-people-fill" />
+						</p>
+						<div class="flex pb-2 items-end gap-1">
+							<p class="text-5xl"><b>24</b></p>
+							<p><b>ta</b></p>
+						</div>
+					</div>
 				</div>
-			{/each}
+				<div
+					class="flex flex-col justify-start gap-3 rounded-xl shadow p-4 bg-white"
+				>
+					<p class="text-2xl font-semibold">All teachers</p>
+					<div class="flex items-end gap-5">
+						<p class="text-7xl text-blue-500">
+							<i class="bi bi-person-workspace" />
+						</p>
+						<div class="flex pb-2 items-end gap-1">
+							<p class="text-5xl"><b>40</b></p>
+							<p><b>ta</b></p>
+						</div>
+					</div>
+				</div>
+				<div
+					class="flex flex-col justify-start gap-3 rounded-xl shadow p-4 bg-white"
+				>
+					<p class="text-2xl font-semibold">All students</p>
+					<div class="flex items-end gap-5">
+						<p class="text-7xl text-blue-500">
+							<i class="bi bi-person-fill" />
+						</p>
+						<div class="flex pb-2 items-end gap-1">
+							<p class="text-5xl"><b>400</b></p>
+							<p><b>ta</b></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<p class="text-xl font-semibold">Groups</p>
+			<div class="bg-white rounded-xl shadow p-5">
+				<table
+					class="w-full table-auto border-collapse border border-black rounded-xl"
+				>
+					<thead>
+						<tr>
+							<th class="border border-slate-600 p-2">№</th>
+							<th class="border border-slate-600 p-2">Name</th>
+							<th class="border border-slate-600 p-2"
+								>Direction</th
+							>
+							<th class="border border-slate-600 p-2">Id</th>
+						</tr>
+						{#each groups as group}
+							<tr>
+								<td class="border border-slate-600 p-2"> {i} </td>
+								<td class="border border-slate-600 p-2"
+									>{group.name}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.direction}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.id}</td
+								>
+							</tr>
+						{/each}
+					</thead>
+				</table>
+			</div>
+		</div>
+		<div>
+			<p class="text-xl font-semibold">Teachers</p>
+			<div class="bg-white rounded-xl shadow p-5">
+				<table
+					class="w-full table-auto border-collapse border border-black rounded-xl"
+				>
+					<thead>
+						<tr>
+							<th class="border border-slate-600 p-2">№</th>
+							<th class="border border-slate-600 p-2">Name</th>
+							<th class="border border-slate-600 p-2"
+								>Direction</th
+							>
+							<th class="border border-slate-600 p-2">Id</th>
+						</tr>
+						{#each groups as group}
+							<tr>
+								<td class="border border-slate-600 p-2">
+									1
+								</td>
+								<td class="border border-slate-600 p-2"
+									>{group.name}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.direction}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.id}</td
+								>
+							</tr>
+						{/each}
+					</thead>
+				</table>
+			</div>
+		</div>
+		<div>
+			<p class="text-xl font-semibold">Students</p>
+			<div class="bg-white rounded-xl shadow p-5">
+				<table
+					class="w-full table-auto border-collapse border border-black rounded-xl"
+				>
+					<thead>
+						<tr>
+							<th class="border border-slate-600 p-2">№</th>
+							<th class="border border-slate-600 p-2">Name</th>
+							<th class="border border-slate-600 p-2"
+								>Direction</th
+							>
+							<th class="border border-slate-600 p-2">Id</th>
+						</tr>
+						{#each groups as group}
+							<tr>
+								<td class="border border-slate-600 p-2">
+									1
+								</td>
+								<td class="border border-slate-600 p-2"
+									>{group.name}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.direction}</td
+								>
+								<td class="border border-slate-600 p-2"
+									>{group.id}</td
+								>
+							</tr>
+						{/each}
+					</thead>
+				</table>
+			</div>
 		</div>
 	</div>
 </section>
---
+++ client/src/routes/Groups.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import axios from "axios";
+
+	interface Group {
+		id:        string,
+		name:      string,
+		direction: string
+	}
+
+	let groups: Group[] = []
+	let group_name: HTMLInputElement
+	let group_direction: HTMLInputElement
+
+	async function getAllGroups() {
+		axios
+			.get('http://localhost:7060/groups')
+			.then(async res => {
+				groups = await res.data.groups
+			})
+	}
+	async function postGroup(name: string, direction: string) {
+		const group = {
+			name,
+			direction
+		}
+		axios
+			.post('http://localhost:7060/groups', group)
+			.then(async res => {
+				alert(await res.data.message)
+				getAllGroups()
+			})
+	}
+	async function deleteGroup(id: string) {
+		axios
+			.delete('http://localhost:7060/groups/' + id)
+			.then(async res => {
+				alert(await res.data.message)
+				getAllGroups()
+			})
+	}
+	
+	getAllGroups()
+</script>
+
+<section class="h-screen flex">
+    <div>
+        <label for="group-name">Enter group name</label>
+		<input
+			bind:this={group_name}
+			class="border-2 p-2 rounded outline-0"
+			type="text"
+			id="group-name"
+			placeholder="Android"
+		/>
+		<label for="group-direction">Enter group direction</label>
+		<input
+			bind:this={group_direction}
+			class="border-2 p-2 rounded outline-0"
+			type="text"
+			id="group-name"
+			placeholder="Info group"
+		/>
+		<button
+			on:click={async () => {
+				await postGroup(group_name.value, group_direction.value);
+			}}
+			class="bg-indigo-600 p-2 rounded text-white">Create group</button
+		>
+    </div>
+	<div class="flex flex-col gap-2 p-5 h-screen border-r-4">
+		<p class="text-xl font-semibold">Create group</p>
+		<label for="group-name">Enter group name</label>
+		<input bind:this={group_name} class="border-2 p-2 rounded outline-0" type="text" id="group-name" placeholder="Android">
+		<label for="group-direction">Enter group direction</label>
+		<input bind:this={group_direction} class="border-2 p-2 rounded outline-0" type="text" id="group-name" placeholder="Info group">
+		<button on:click={async () => {await postGroup(group_name.value, group_direction.value)}}  class="bg-indigo-600 p-2 rounded text-white">Create group</button>
+	</div>
+	<div class="p-5 bg-[#e2e8f0] w-full">
+		<p class="text-xl font-semibold">All groups</p>
+		<div class="grid gap-3 grid-cols-4 py-5">
+			{#each groups as group}
+				<div class="flex flex-col justify-start gap-3 rounded-xl shadow p-4 bg-white">
+					<p><b>Id: </b>{group.id}</p>
+					<p><b>Group: </b>{group.name}</p>
+					<p><b>Direction: </b>{group.direction}</p>
+					<button on:click={async () => {await deleteGroup(group.id)}} class="text-white bg-red-600 p-2 rounded">O'chirish</button>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
